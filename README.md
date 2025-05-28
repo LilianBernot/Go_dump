f79da3b1992170df9bd99ce395cb43ab566f6467
LilianBernot <lilian.bernot@gmail.com>
2025-05-28 16:31:35 -0400

    Implement ls

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-This is a small repo to add some go projects to discover the language !
+# This is a small repo to add some go projects to discover the language !
+
+## First project : ls_lib !
+
+If you want to be able to use it from anywhere in your computer, just run : sudo mv myls /usr/local/bin/.
--- ls_lib/ls.go
+++ ls_lib/ls.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
+// This package will be an implementation of the ls function in go !
 func main() {
-    fmt.Println("This package will be an implementation of the ls function in go !")
+	dir := "."
+
+	// Read the directory
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Print the file and folder names
+	for _, entry := range files {
+		name := entry.Name()
+
+		if entry.IsDir() {
+			// Blue color for directories
+			fmt.Printf("\033[34m%s\033[0m\n", name)
+		} else {
+			// Default color for files
+			fmt.Println(name)
+		}
+	}
 }
