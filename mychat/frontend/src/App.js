3ddeb7d76e14fe40de5be87ae96b76a60d521c9d
LilianBernot <lilian.bernot@gmail.com>
2025-05-30 10:01:13 -0400

    Mychat : create header

--- mychat/frontend/src/App.js
+++ mychat/frontend/src/App.js
@@ -1,28 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 import { connect, sendMsg } from './api';
 import React, { Component } from "react";
+import Header from './components/header';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 class App extends Component {
   constructor(props) {
@@ -38,6 +18,7 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
+        <Header/>
         <button onClick={this.send}>Hit</button>
       </div>
     )
---
+++ mychat/frontend/src/components/header/header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => (
+    <div className="header">
+        <h2>My realtime chat app</h2>
+    </div>
+);
+
+export default Header;
